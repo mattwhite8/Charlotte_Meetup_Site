eb26ec8367268d48cb2a08bdbc8c2769648b9591
Matt White <mattwhite1337@gmail.com>
2017-01-08 18:43:23 -0500

    add jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,59 @@
-$('#featured').carousel({
-  interval: false
+$(function() {
+
+	"use strict";
+
+	var topOffset = 50; //variable for menu height
+	var slideqty = ('#featured .item').length;
+
+	//Activate Scrollspy
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topOffset
+	});
+
+	var hash = $(this).find('li.active a').attr('href');
+	if(hash !== '#featured') {
+		$('header nav').addClass('inbody');
+	} else {
+		$('header nav').removeClass('inbody');
+	}
+
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+		var hash = $(this).find('li.active a').attr('href');
+		if(hash !== '#featured') {
+			$('header nav').addClass('inbody');
+		} else {
+			$('header nav').removeClass('inbody');
+		}
+	});
+
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') === 
+		  this.pathname.replace(/^\//,'') && 
+		  location.hostname === this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+		    $('html,body').animate({
+		      scrollTop: target.offset().top-topOffset+2
+		    }, 500);
+		    return false;
+		  } //target.length
+		} //click function
+	}); //smooth scrolling	
+
+
+	//Automatically generate carousel indicators
+	for(var i = 0; i < slideqty; i++){
+		var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+		$('#featured ol').append(insertText);
+	}
+
+	$('#featured').carousel({
+  		interval: false
+	});
+
 });
+
+
