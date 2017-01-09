75f6da0a284e187d75d5db82065394510544d909
Matt White <mattwhite1337@gmail.com>
2017-01-08 19:54:14 -0500

    version 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,86 @@
 $(function() {
 
-	"use strict";
+  "use strict";
 
-	var topOffset = 50; //variable for menu height
-	var slideqty = ('#featured .item').length;
+  var topoffset = 50; //variable for menu height
+  var slideqty = $('#featured .item').length;
+  var wheight = $(window).height(); //get the height of the window
+  var randSlide = Math.floor(Math.random()*slideqty);
 
-	//Activate Scrollspy
-	$('body').scrollspy({
-		target: 'header .navbar',
-		offset: topOffset
-	});
+  $('#featured .item').eq(randSlide).addClass('active');
 
-	var hash = $(this).find('li.active a').attr('href');
-	if(hash !== '#featured') {
-		$('header nav').addClass('inbody');
-	} else {
-		$('header nav').removeClass('inbody');
-	}
-
-	$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
-		var hash = $(this).find('li.active a').attr('href');
-		if(hash !== '#featured') {
-			$('header nav').addClass('inbody');
-		} else {
-			$('header nav').removeClass('inbody');
-		}
-	});
-
-	//Use smooth scrolling when clicking on navigation
-	$('.navbar a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') === 
-		  this.pathname.replace(/^\//,'') && 
-		  location.hostname === this.hostname) {
-		  var target = $(this.hash);
-		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		  if (target.length) {
-		    $('html,body').animate({
-		      scrollTop: target.offset().top-topOffset+2
-		    }, 500);
-		    return false;
-		  } //target.length
-		} //click function
-	}); //smooth scrolling	
+  $('.fullheight').css('height', wheight); //set to window tallness  
 
 
-	//Automatically generate carousel indicators
-	for(var i = 0; i < slideqty; i++){
-		var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
-		$('#featured ol').append(insertText);
-	}
+  //replace IMG inside carousels with a background image
+  $('#featured .item img').each(function() {
+    var imgSrc = $(this).attr('src');
+    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+    $(this).remove();
+  });
 
-	$('#featured').carousel({
-  		interval: false
-	});
+  //adjust height of .fullheight elements on window resize
+  $(window).resize(function() {
+    wheight = $(window).height(); //get the height of the window
+    $('.fullheight').css('height', wheight); //set to window tallness  
+  });
+
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: 'header .navbar',
+    offset: topoffset
+  });
+
+  // add inbody class
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#featured') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+
+  // Add an inbody class to nav when scrollspy event fires
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  //Automatically generate carousel indicators
+  for (var i=0; i < slideqty; i++) {
+    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+    if (i === randSlide) {
+      insertText += ' class="active" ';
+    }
+    insertText += '></li>';
+    $('#featured ol').append(insertText);
+  }
+
+  $('.carousel').carousel({
+    pause: false
+  });
 
 });
-
 
